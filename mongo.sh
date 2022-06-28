1eddd1a2f321381f6dc2f838657a0baa38395083
123name <123@hotmail.com>
2022-06-28 07:25:10 +0000

    wrote messy script to install mongodb from tgz tar-ball, manual steps are still required to download the .tgz and some other initial set up steps, comments in mongo.sh give basic steps

--- mongo.sh
+++ mongo.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-mongod --dbpath /var/lib/mongo --logpath /var/log/mongodb/mongod.log & disown
+#sudo apt-get install libcurl4 openssl liblzma5
+#tar -zxvf mongodb-linux-x86_64-ubuntu1804-5.0.9.tgz
+#sudo cp <mongodb-install-dir>/bin/* /usr/local/bin/ 
+
+sudo mkdir -p /var/lib/mongo
+sudo mkdir -p /var/log/mongodb
+
+sudo chown `whoami` /var/lib/mongo
+sudo chown `whoami` /var/log/mongodb
+
+
+
 
 
 
---
+++ start_mongo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mongod --dbpath /var/lib/mongo --logpath /var/log/mongodb/mongod.log & disown
+
+
+
+
